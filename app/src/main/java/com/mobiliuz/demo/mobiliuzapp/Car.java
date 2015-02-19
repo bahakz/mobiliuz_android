86e5bcf6b1db8f55cde84e00dc9d76f41021cfb3
mukatay <mukatay.darhan@gmail.com>
2015-02-19 15:22:58 +0600

    test commit

--- app/src/main/java/com/mobiliuz/demo/mobiliuzapp/Car.java
+++ app/src/main/java/com/mobiliuz/demo/mobiliuzapp/Car.java
@@ -6,8 +6,6 @@ package com.mobiliuz.demo.mobiliuzapp;
 public class Car {
 
     private int id;
-    private String model;
-    private String fuelConsumption;
     private double powerVoltage;
     private double latitude;
     private double longitude;
@@ -24,22 +22,6 @@ public class Car {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getFuelConsumption() {
-        return fuelConsumption;
-    }
-
-    public void setFuelConsumption(String fuelConsumption) {
-        this.fuelConsumption = fuelConsumption;
     }
 
     public double getPowerVoltage() {
